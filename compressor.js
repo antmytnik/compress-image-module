662ad1d3da22c7a83b771a1704d33044b5580733
AntonMytnik <AntMytnik@gmail.com>
2022-05-25 14:01:45 +0200

    fixed bugs

--- compressor.js
+++ compressor.js
@@ -2,18 +2,15 @@ function compressAll(item_id = null, width = null, height = null, compress_lvl =
     var compress_array = [];
     if (item_id !== null && typeof item_id === 'object') {
         try {
-            var p1 = new Promise(function(resolve, reject) {
-                for (var i = 0, numFiles = item_id.files.length; i < numFiles; i++) {
-                    compress_array.push(compressImage(item_id.files[i], height, width, compress_lvl, option));
-                }
-                // console.log(item_id.files.length);
-                setTimeout(() => resolve(compress_array), 2000);
-                // resolve(compress_array);
-            });
-            return (p1.then(function(value) {
-                console.log(value);
-                return value;
-            }));
+            for (var i = 0, numFiles = item_id.files.length; i < numFiles; i++) {
+                var main_pr = new Promise(function(resolve, reject) {
+                    resolve(compressImage(item_id.files[i], height, width, compress_lvl, option));
+                });
+                main_pr.then(function(value) {
+                    compress_array.push(value);
+                });
+            }
+            return compress_array;
         } catch (err) {
             console.error(err);
         }
@@ -21,17 +18,12 @@ function compressAll(item_id = null, width = null, height = null, compress_lvl =
     } else return false;
 
     async function compressImage(img, width, height, compress_lvl, option) {
-
         var rere = new Promise(function(resolve, reject) {
             var reader = new FileReader();
             selectedFile = img;
             reader.onload = function(event) {
                 var imgtag = new Image();
-
                 imgtag.src = event.target.result;
-                // imgtag.height = height;
-                console.log(selectedFile);
-                // document.body.append(imgtag);
                 var objectUrl = URL.createObjectURL(img);
                 imgtag.onload =
                     function() {
@@ -52,10 +44,6 @@ function compressAll(item_id = null, width = null, height = null, compress_lvl =
                             original_width = width;
                         }
                         URL.revokeObjectURL(objectUrl);
-                        console.log(original_height);
-                        console.log(original_width);
-
-
                         var file_data = null;
                         var can = document.createElement("canvas");
                         can.width = original_width;
@@ -67,57 +55,11 @@ function compressAll(item_id = null, width = null, height = null, compress_lvl =
                         document.body.append(img);
                         resolve(file_data);
                     };
-
-
-
             };
             reader.readAsDataURL(img);
         });
-        rere.then(function(value) {
-            console.log(value);
+        return rere.then(function(value) {
             return value;
         });
-
-        // var original_height, original_width;
-
-        // var img_proto = new Image(255, 255);
-        // img_proto.src = img.name;
-        // console.log(img_proto);
-        // document.body.append(img_proto);
-
-        // var objectUrl = _URL.createObjectURL(img);
-        // var rere = new Promise(function(resolve, reject) {
-        //     img_proto.onload =
-        //         function() {
-        //             console.log('Promise start');
-        //             original_height = this.height;
-        //             original_width = this.width;
-        //             _URL.revokeObjectURL(objectUrl);
-        //             console.log(original_height);
-        //             resolve(original_height);
-        //         };
-        // });
-        // rere.then(function(value) {
-        //     console.log(value);
-        //     return value;
-        // });
-        // console.log('Promise ende');
-
-        // var file_data = $("#category_image").prop('files')[index];
-        // var can = document.createElement("canvas");
-        // can.width = img.width * 2.5;
-        // can.height = img.height * 2.5;
-        // var ctx = can.getContext("2d");
-        // ctx.drawImage(img, 0, 0, can.width, can.height);
-        // file_data = can.toDataURL('image/jpeg', 100);
-        // img.src = file_data;
-        // if (index == 0) {
-        //     mainImage = file_data;
-        // } else {
-        //     otherImage[index - 1] = file_data;
-        // }
-        // JotherImage = JSON.stringify(otherImage);
-        // can.remove();
-
     }
 }
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 function pasteData() {
+    console.info("Yor compressed array:");
     console.log(compressAll(document.getElementById('main_image'), 600, 600, 100, 'contain'));
 }
